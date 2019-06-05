7bec5e254e5982b160fb47d539c33481b92068b8
kaldren <drenski666@gmail.com>
2019-06-05 20:58:17 +0300

    Updated Comments content to string

--- SmallDad/Models/Comment.cs
+++ SmallDad/Models/Comment.cs
@@ -8,7 +8,7 @@ namespace SmallDad.Models
     public class Comment
     {
         public int Id { get; set; }
-        public int Content { get; set; }
+        public string Content { get; set; }
         public string AuthorId { get; set; }
         public ApplicationUser Author { get; set; }
         public int Rankid { get; set; }
