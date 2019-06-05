03c5ab845e92a321e97d30415cc44239cc93a524
kaldren <drenski666@gmail.com>
2019-06-05 20:09:48 +0300

    Comment model

---
+++ SmallDad/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmallDad.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int Content { get; set; }
+        public string AuthorId { get; set; }
+        public ApplicationUser Author { get; set; }
+    }
+}
