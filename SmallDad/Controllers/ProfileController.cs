4242caf4fe7be57bae255ecb18f45e55814f8cd5
kaldren <drenski666@gmail.com>
2019-06-06 21:07:57 +0300

    Profile basic view

--- SmallDad/Controllers/ProfileController.cs
+++ SmallDad/Controllers/ProfileController.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SmallDad.Controllers
 {
-    public class ProfileController
+    public class ProfileController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
