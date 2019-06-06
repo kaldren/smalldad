365b0d4ce6dcb7ec08dd226e7a10f357f41fa94b
kaldren <drenski666@gmail.com>
2019-06-06 21:26:46 +0300

    Profile page

--- SmallDad/Controllers/ProfileController.cs
+++ SmallDad/Controllers/ProfileController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmallDad.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,26 @@ namespace SmallDad.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("/Profile/{id:guid}")]
+        public async Task<IActionResult> GetProfileById(Guid id)
+        {
+            var profile = await _context.Users
+                            .Where(x => x.Id == id.ToString())
+                            .SingleOrDefaultAsync();
+
+            return View(profile);
         }
     }
 }
