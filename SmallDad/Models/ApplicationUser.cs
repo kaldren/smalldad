0454e113a08544dc0e5cce05f84795392a16b30a
kaldren <drenski666@gmail.com>
2019-06-06 21:29:59 +0300

    Added first name and last name to profile

--- SmallDad/Models/ApplicationUser.cs
+++ SmallDad/Models/ApplicationUser.cs
@@ -8,6 +8,8 @@ namespace SmallDad.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Biography { get; set; }
         public List<Comment> Comments { get; set; }
     }
