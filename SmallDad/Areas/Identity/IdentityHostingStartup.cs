fc977cf9c7b3f46c26c57af4f23a1f85449b15b7
kaldren <drenski666@gmail.com>
2019-06-07 06:38:08 +0300

    Added profile biography

---
+++ SmallDad/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmallDad.Data;
+using SmallDad.Models;
+
+[assembly: HostingStartup(typeof(SmallDad.Areas.Identity.IdentityHostingStartup))]
+namespace SmallDad.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
