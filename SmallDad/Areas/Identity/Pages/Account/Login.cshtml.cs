b8afa26c3c968875cc42a482676742406b37a62a
kaldren <drenski666@gmail.com>
2019-06-08 12:59:53 +0300

    Comment

--- SmallDad/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SmallDad/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -72,11 +72,10 @@ namespace SmallDad.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var userToLogin = await _signInManager.UserManager.FindByEmailAsync(Input.Email);
 
-                var result = await _signInManager.PasswordSignInAsync(userToLogin, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(userToLogin, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
---
+++ SmallDad/Controllers/CommentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmallDad.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmallDad.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
