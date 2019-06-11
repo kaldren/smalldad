2cedfbd25e0af07e53dcd9d83f34696ab85c6469
kaldren <drenski666@gmail.com>
2019-06-11 20:26:37 +0300

    ProfileViewModel updated

--- SmallDad/ViewModels/Profile/ProfileViewModel.cs
+++ SmallDad/ViewModels/Profile/ProfileViewModel.cs
@@ -7,5 +7,11 @@ namespace SmallDad.ViewModels.Profile
 {
     public class ProfileViewModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Biography { get; set; }
+        public string UserName { get; set; }
+        public string ProfilePhotoPath { get; set; }
+        public string ProfilePhotoThumbPath { get; set; }
     }
 }
