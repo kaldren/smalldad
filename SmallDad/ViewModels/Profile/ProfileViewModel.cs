3163844b4f9a65caaccbd1a9f609991564b54918
kaldren <drenski666@gmail.com>
2019-06-11 20:25:08 +0300

    ProfileViewModel

---
+++ SmallDad/ViewModels/Profile/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmallDad.ViewModels.Profile
+{
+    public class ProfileViewModel
+    {
+    }
+}
